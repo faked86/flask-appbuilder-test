9ad048e84fc7c0c62dddaf1a970f7bd99d2bbe65
Dan <ulturgashev112@gmail.com>
2024-04-19 09:19:57 +0300

    fix auth_user_db

--- app/security.py
+++ app/security.py
@@ -4,7 +4,7 @@ from flask_appbuilder.security.sqla.manager import SecurityManager
 class MySecurityManager(SecurityManager):
 
     def auth_user_db(self, username, password):
-        user = self.find_user(username=username)
-        if password == "admin":
+        if username == "admin" and password == "admin":
+            user = self.find_user(username=username)
             return user
         return None
